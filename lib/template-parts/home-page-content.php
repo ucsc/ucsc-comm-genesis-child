37d595bc60b90b415898611f4a76f739a6d76788
Jason Chafin <jason@blackbirdconsult.com>
2017-11-19 10:34:49 -0800

    working on home page hero

--- lib/template-parts/home-page-content.php
+++ lib/template-parts/home-page-content.php
@@ -1,8 +1,8 @@
 <?php
-// remove_action( 'genesis_after_header', 'genesis_do_nav' );
-// remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
-// remove_action( 'genesis_header', 'genesis_do_header' );
-// remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+remove_action( 'genesis_header', 'genesis_do_header' );
+remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
 //* Replace Content with Custom Loop
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop',  __NAMESPACE__ .'\bb_front_page_content_loop' );
@@ -11,13 +11,14 @@ function bb_front_page_content_loop() {
     
     echo "Hello World";
 }
-//add_action ('genesis_after_header',__NAMESPACE__ . '\bb_home_hero', 20);
+add_action ('genesis_after_header',__NAMESPACE__ . '\bb_home_hero', 20);
 
 function bb_home_hero() {
     
- 
     echo '<div class= "hero-container">';
+    echo '<div class= "hero-nav-container">';
     echo '<div class= "wrap">';
+    echo '<div class= "hero-nav">';
     wp_nav_menu( array(
         'menu' => 'after-header-menu',
         'menu_class' => 'menu genesis-nav-menu menu-primary js-superfish sf-js-enabled sf-arrows',
@@ -29,6 +30,37 @@ function bb_home_hero() {
         
           ) );
     
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class=hero-banner-container>';
+    echo '<div class= "wrap">';
+    echo '<div class= "hero-banner">';
+    $headerLogo = get_stylesheet_directory_uri().'/assets/images/UC_Santa_Cruz_Horizontal.png';
+    echo'<img class="" src="'.$headerLogo.'" alt="UCSC Logo" >';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class=hero-title-container>';
+    echo '<div class= "wrap">';
+    echo '<div class= "hero-title">';
+    $headerTitle = get_bloginfo( 'name' );
+    $headerDescription = get_bloginfo( 'description' );
+    $headerUrl = get_bloginfo( 'url' );
+    
+    echo '<div class="title-area"><p class="site-title" itemprop="headline"><a href="'.$headerUrl.'">'.$headerTitle.'</a></p><p class="site-description screen-reader-text" itemprop="description">'.$headerDescription.'</p></div>';
+
+
+
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class=hero-chevron-container>';
+    echo '<div class= "wrap">';
+    echo '<div class= "hero-chevron">';
+    echo "Hello World";
+    echo '</div>';
+    echo '</div>';
     echo '</div>';
     echo '</div>';
 }
