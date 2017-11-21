12bb24fc585927cb3194e1bdce598f27616afd3a
Jason Chafin <jason@blackbirdconsult.com>
2017-11-21 15:55:29 -0800

    Ipsum Lorem

--- lib/structure/before-header.php
+++ lib/structure/before-header.php
@@ -21,7 +21,7 @@ function bb_add_top_row(){
 	$logo = get_stylesheet_directory_uri().'/assets/images/logo-abbr.png';
 	
 	$custom_title = '<a href="/"><img class="header-logo" src="'.$logo.'" alt="UCSC Logo" ></a>';
-	echo '<div class="page-top">';
+	echo '<div class="page-top" id="page-top">';
 	echo '<div class="top-row wrap">';
 	
 	echo '<div class="top-row-left">';
